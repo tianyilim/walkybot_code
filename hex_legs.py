3a6ecde1c5ff12a93bb8e3849b5bc0fbc92fe87a
tianyilim <0.tianyi.lim@gmail.com>
2020-09-02 16:39:13 +0800

    added variable scope to drivers

--- hex_legs.py
+++ hex_legs.py
@@ -11,103 +11,141 @@ from adafruit_servokit import ServoKit
 import warnings # Tells if we are doing something wrong
 
 class legJoint:
+    # "Public" variables
+    curr_angle = 90
+
+    # "Private" variables
     _servo_num = 0
     _offset = 0 # can be tuned for non-idealites in hardware setup
     _limit = 30 # Limit to motion, servo cannot pass this value
-    _start_angle = 90
-    _angle_max = curr_angle + limit
-    _angle_min = curr_angle - limit
+    _start_angle = curr_angle
+    _angle_max = curr_angle + _offset + _limit
+    _angle_min = curr_angle + _offset - _limit
     
     _servo_driver = None # empty variable for now
 
-    curr_angle = 0 # Current angle to be written to servo
-
     def __init__(self, kit, servo_num, start_angle=90, limit=30, offset=0):
         # Initialises servo values. Here, start angle refers to the neutral position of the servo.
-        _servo_num = servo_num
-        _offset = offset
-        _limit = limit
-        _start_angle = start_angle + _offset
-        _angle_max = _start_angle + _limit
-        _angle_min = _start_angle - _limit
+        self._servo_num = servo_num
+        self._offset = offset
+        self._limit = limit
+        self._start_angle = start_angle + self._offset
+        self._angle_max = self._start_angle + self._limit
+        self._angle_min = self._start_angle - self._limit
 
-        _servo_driver = kit.servo[servo_num]
-
-        curr_angle = _start_angle # init this stuff.
+        self._servo_driver = kit.servo[servo_num]
 
     def writeLimit(self, limit):
         # Updates the limits of the object.
-        _limit = limit
-        _angle_max = _start_angle + _limit
-        _angle_min = _start_angle - _limit
+        self._limit = limit
+        self._angle_max = self._start_angle + self._offset + self._limit
+        self._angle_min = self._start_angle + self._offset - self._limit
         
-        return _angle_max, _angle_min
+        return self._angle_max, self._angle_min
 
     def writeAngle(self, angle):
-        try_angle = angle + offset
-        if try_angle > _angle_max:
-            curr_angle = _angle_max
+        try_angle = angle + self._offset
+        if try_angle > self._angle_max:
+            self.curr_angle = self._angle_max
             warnings.warn("%s: Exceeded +ve limit" %self.__class__.__name__)
-        elif try_angle < angle_min:
-            curr_angle = _angle_min
+        elif try_angle < self._angle_min:
+            self.curr_angle = self._angle_min
             warnings.warn("%s: Exceeded -ve limit" %self.__class__.__name__)
         else:
-            curr_angle = try_angle
+            self.curr_angle = try_angle
 
-        _servo_driver.angle = curr_angle # Write servo angle here.
+        self._servo_driver.angle = self.curr_angle # Write servo angle here.
 
-        return curr_angle
+        return self.curr_angle
 
-
-# initialize everything from hexleg.
-# current xyz and desired xyz can go here too.
+# Contains the Leg IK
 class hexLeg:
-    coxa_num = 0
-    tibia_num = 0
-    femur_num = 0
+    # Private variables
+    _coxa_num = 0
+    _tibia_num = 0
+    _femur_num = 0
+    _left_right = True
+
+    # Public variables
+    coxa = None
+    tibia = None
+    femur = None
+    joints = (coxa, tibia, femur)
 
     def __init__(self, kit, legNum, leftright):
-        # Attaches the servos (assumes that left and right control boards have been attached)
-        # Problem: How to address the left/right servo objects if they are defined in a higher class?
+        # Provides arguments for the attachment of the individual servos
         if leftright == 'left':
-            coxa_num = 15 - (legNum-1)*4 # 1: 15, 2: 11, 3: 7
-            tibia_num = coxa_num - 1
-            femur_num = coxa_num - 2
+            self._left_right = True
+            self._coxa_num = 15 - (legNum-1)*4 # 1: 15, 2: 11, 3: 7
+            self._tibia_num = self._coxa_num - 1
+            self._tibia_num = self._coxa_num - 2
 
         elif leftright == 'right':
-            coxa_num = (legNum-1)*4 # 1: 0, 2: 4, 3: 8
-            tibia_num = coxa_num + 1
-            femur_num = coxa_num + 2
+            self._left_right = False
+            self._coxa_num = (legNum-1)*4 # 1: 0, 2: 4, 3: 8
+            self._tibia_num = self._coxa_num + 1
+            self._tibia_num = self._coxa_num + 2
             
         else:
             raise NameError("Invalid argument in leftright")
+
+        self.coxa = legJoint(kit, self._coxa_num)
+        self.tibia = legJoint(kit, self._tibia_num)
+        self.femur = legJoint(kit, self._tibia_num) # Attach these things.
+
+    def update_limits(self, limits):
+        if len(limits) == len(self.joints):
+            for x in range(len(self.joints)):
+                self.joints[x].writeLimit(limits[x])
+            return True
+        else:
+            return False
+    
+    # def update_limits(self, limit_coxa, limit_tibia, limit_femur):
+    #     self.coxa.writeLimit(limit_coxa)
+    #     self.tibia.writeLimit(limit_tibia)
+    #     self.femur.writeLimit(limit_femur)
+
+    def write_angles(self, angles):
+        if len(angles) == len(self.joints):
+            for x in range(len(self.joints)):
+                self.joints[x].writeAngle(angles[x])
+            return True
+        else:
             return False
 
-        coxa = legJoint(kit, coxa_num)
-        tibia = legJoint(kit, tibia_num)
-        femur = legJoint(kit, femur_num) # Attach these things.
-
-    return True
-
-
+# Starts the servo driver.
 class legDriver:
+    # Public variables
+    leg1 = None
+    leg2 = None
+    leg3 = None
+    # Private Variables
     
+    # attaches all legs to the servo controller.
     def __init__(self, left_right, addr):
         if left_right != 'left' or left_right != 'right':
             raise NameError("Invalid argument in left_right")
-            return False
         else:
             kit = ServoKit(channels=16, address=addr)
-            leg_1 = hexLeg(kit, legNum=1, leftright=left_right) # attach all servos here
-            leg_2 = hexLeg(kit, legNum=2, leftright=left_right)
-            leg_3 = hexLeg(kit, legNum=3, leftright=left_right)
-
-            return True
+            self.leg1 = hexLeg(kit, legNum=1, leftright=left_right) # attach all servos here
+            self.leg2 = hexLeg(kit, legNum=2, leftright=left_right)
+            self.leg3 = hexLeg(kit, legNum=3, leftright=left_right)
 
 
-# Initialises all 6 legs' hardware here.
-# Current xyz, roll/pitch/yaw go here.
+# Contains the body IK
 class hexapod:
+    # Private variables
+    
+    # Public variables
+    left_legs = None
+    right_legs = None
+    legs = None
+
     def __init__(self, left_addr=0x42, right_addr=0x41):
-        left_legs = legDriver('left', addr=left_addr)
-        right_legs = legDriver('right', addr=right_addr)
+        self.left_legs = legDriver('left', addr=left_addr)
+        self.right_legs = legDriver('right', addr=right_addr)
+
+        # List up
+        self.legs = (self.right_legs.leg1, self.right_legs.leg2, self.right_legs.leg3,
+                self.left_legs.leg3, self.left_legs.leg2, self.left_legs.leg1)
