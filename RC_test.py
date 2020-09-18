1da05d356f6ae48c4a3bc9d7959943bfe47e129d
tianyilim <0.tianyi.lim@gmail.com>
2020-09-18 20:21:52 +0800

    rc implementation

---
+++ RC_test.py
@@ -0,0 +1,77 @@
+''' Socket server, receives (RX) '''
+import socket
+import sys
+# import hex_legs
+import hexapod
+import numpy as np
+from adafruit_servokit import ServoKit
+import time
+
+# Helper function
+def int_twosComp(val, bits=8):
+    if val > 2**(bits-1)-1:
+        return val - 2**bits
+    else:
+        return val
+
+SCALE = 256/30
+RATE = 256/100
+translate = [0.0,0.0] # as a proportion
+tilt = [0.0,0.0]
+rotate = 0
+updown = 0
+
+target_name = "192.168.1.80"
+target_port = 10000
+server_address = (target_name, target_port)
+
+# initialise hexapod object
+leg_end_coords = np.tile((0.0, 68.75, 0.0), (6,1)) # check back
+leg_angles = np.tile(90.0, 6)  # leg angle init
+# print(leg_end_coords, leg_end_coords.shape)
+walky = hexapod.hexapod(leg_end_loc=leg_end_coords, leg_angle=leg_angles)
+
+# Create a UDP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.settimeout(5) # Use this, easier than to make sockets nonblocking
+# Bind the socket to the port
+print('starting up on %s port %s' % server_address)
+sock.bind(server_address)
+
+while True:
+    # print('\nwaiting to receive message')
+    data, address = sock.recvfrom(4096)
+    if data:
+        # print( 'received %s bytes from %s' % (len(data), address) )
+        
+        # for i,x in enumerate(data):
+        #     print(i, int.from_bytes(x, byteorder='big', signed=True))
+
+        translate[0] = int_twosComp(data[0]) / RATE
+        translate[1] = int_twosComp(data[1]) / RATE
+        tilt[0] = int_twosComp(data[2]) / SCALE
+        tilt[1] = int_twosComp(data[3]) / SCALE
+        rotate = int_twosComp(data[4]) / SCALE
+        updown = int_twosComp(data[5]) / 2
+
+        walky.body_pitch(tilt[0])
+        walky.body_roll(tilt[1])
+        walky.body_rotate_absolute(rotate)
+        walky.body_translate_z(updown)
+        walky.update_legs_stance()        
+
+        #debug
+        print("Translate values:", translate)
+        print("Tilt values:", tilt)
+        print("Rotate:", rotate)
+        if updown == 1:
+            print("Increase Z")
+        elif updown == -1:
+            print("Lower Z")
+        else:
+            print("Keep Z")
+        print("")
+
+sock.shutdown()
+sock.close()
+sys.exit()
---
+++ remote_control.py
@@ -0,0 +1,100 @@
+''' Controls the RPi robot via UDP sockets '''
+from inputs import get_gamepad
+import time
+import socket
+import sys
+from threading import Thread
+
+# Gamepad inputs
+DPAD_MAX = 32767
+DPAD_SCALE = DPAD_MAX/127
+TRIG_MAX = 255
+TRIG_SCALE = TRIG_MAX/127
+translate = [0,0] # as a proportion
+tilt = [0,0]
+rotate = 0
+updown = 0
+
+target_name = "raspberrypi.local" # Check this
+target_port = 10000
+server_address = (target_name, target_port)
+
+def int_twosComp(val, bits=8):
+    if val > 2**(bits-1)-1:
+        return val - 2**bits
+    else:
+        return val
+
+def controllerInput():
+    rotateRight = 0.0
+    rotateLeft = 0.0
+    global translate
+    global tilt
+    global rotate
+    global updown
+
+    while True:
+        events = get_gamepad()
+
+        for event in events:
+            # print(event.ev_type, event.code, event.state)
+
+            if event.code == "ABS_X":
+                translate[1] = int(event.state / DPAD_SCALE)
+            if event.code == "ABS_Y":
+                translate[0] = int(event.state / DPAD_SCALE)
+            if event.code == "ABS_RX":
+                tilt[1] = int(event.state / DPAD_SCALE)
+            if event.code == "ABS_RY":
+                tilt[0] = int(event.state / DPAD_SCALE)
+            if event.code == "ABS_Z":
+                rotateLeft = event.state / TRIG_SCALE
+            if event.code == "ABS_RZ":
+                rotateRight = event.state / TRIG_SCALE
+            if event.code == "ABS_HAT0Y":
+                if event.state == -1:
+                    updown = 1
+                elif event.state == 1:
+                    updown = -1
+                else:
+                    updown = 0
+        rotate = int(rotateRight - rotateLeft)
+
+def transmitInput(target_params):
+    global translate
+    global tilt
+    global rotate
+    global updown
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Create a UDP socket
+    sock.settimeout(5)
+    
+    while True:
+        # Formatting data into a byte array
+        data = b""
+        data += bytes( (translate[0]).to_bytes(1,'little', signed=True) )
+        data += bytes( (translate[1]).to_bytes(1,'little', signed=True) )
+        data += bytes( (tilt[0]).to_bytes(1,'little', signed=True) )
+        data += bytes( (tilt[1]).to_bytes(1,'little', signed=True) )
+        data += bytes( (rotate).to_bytes(1,'little', signed=True) )
+        data += bytes( (updown).to_bytes(1,'little', signed=True) )
+        
+        sent = sock.sendto(data, target_params)
+        
+        # Debug
+        print("Sent %s bytes" %sent)
+        for i, x in enumerate(data):
+            print(i, int_twosComp(x) )
+        time.sleep(0.01) # Don't need to update so often
+    
+print("Starting stuff")
+inputThread = Thread(target=controllerInput)
+transmitThread = Thread(target=transmitInput, args=(server_address,))
+inputThread.daemon=True
+transmitThread.daemon=True
+inputThread.start()
+transmitThread.start()
+
+while True:
+    if not KeyboardInterrupt:
+        sys.exit()  #placeholder for testing, remove these when inserting hexapod code 
